bfcac3822981f13ed27226673fb7430895dd9405
InnaPrincess <innadrake@gmail.com>
2021-10-05 00:15:32 +0300

    inna

--- index.php
+++ index.php
@@ -6,11 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-<body><p style = "color : red" >
-<?php 
-    $pop = 99888954646450;
-    echo $pop;
-?></p>
-    
+<body>
+    <script>
+let $number_arrey = [9900, 87, 33];
+if (1 != 10)
+for (let $num of $number_arrey) {
+let $arifmetic = $num * 1;
+console.log($arifmetic);
+}
+    </script>
+    <?php
+    $tyuu = 1;
+    echo  $tyuu;
+    echo $tyuu + 199990000;
+    ?>
 </body>
 </html>
