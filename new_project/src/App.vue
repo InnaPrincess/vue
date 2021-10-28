601980452208ab86b5916120d58e602783c263b5
InnaPrincess <innadrake@gmail.com>
2021-10-28 13:24:08 +0300

    node

---
+++ new_project/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>кол11er00000 лайк {{ likes }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      likes: 0
+    }
+  } 
+}
+</script>
+
+<style></style>
